c2cb44db5e9b93236dc11c68c927c8927b62d138
crosspatch <christine.d.lynch@gmail.com>
2017-06-06 22:32:34 -0400

    I think it's finished! WOO HOO!

--- Lab01.java
+++ Lab01.java
@@ -1,5 +1,5 @@
 //First Lab 
-import java.util.*; 
+import java.util.*;
 import java.io.*; 
 public class Lab01{ 
     //first search method 
@@ -78,13 +78,25 @@ public class Lab01{
                 listTwo.add(y.next());
             }
 
-            int low = 0;
-            int high = listOne.size() - 1;
-            int middle = (low + high + 1)/2;
-            int location = -1;
-
             for(int i=0; i< listOne.size(); i++){
+				        int low = 0;
+				        int high = listOne.size() - 1;
+				        int middle = (low + high + 1)/2;
+				        int location = -1;
+
                 do{
+
+								// Debugging print statements.
+								/*System.out.println("Searching for: " + listOne.get(i));
+								for(int j = 0; j < listTwo.size(); j++) {
+									System.out.print(listTwo.get(j) + " ");
+									if(j == low) System.out.print("(low) ");
+									if(j == middle) System.out.print("(mid) ");
+									if(j == high) System.out.print("(hi) ");
+								}
+								System.out.println();*/
+
+
                 String curr_key = listOne.get(i);
                 String curr_middle = listTwo.get(middle);
                 if(curr_key.compareToIgnoreCase(curr_middle) == 0)
@@ -96,19 +108,96 @@ public class Lab01{
                 middle =(low + high + 1)/2;
                 }
                 while((low <= high) && (location == -1));
-                System.out.println(listOne.get(location));
+								if(location != -1) System.out.println(listTwo.get(location));
             }
             x.close();
             y.close();
         }
-        catch(Exception e){
+        catch(FileNotFoundException e){
             System.out.println("Could not read file");
         }
 
     
     }
-   /* public void findByFinese(String fileOne, String fileTwo){
-    }*/
+    public static void findByFinesse(String fileOne, String fileTwo){
+
+        System.out.println("The method findByFinesse found the following match(es):");
+
+        String Marker_One;
+        String Marker_Two;
+        ArrayList<String> list_One = new ArrayList<String>();
+        ArrayList<String> list_Two = new ArrayList<String>();
+
+        Scanner x;
+        Scanner y;
+
+        try{
+            x = new Scanner(new File(fileOne));
+            y = new Scanner(new File(fileTwo));
+
+            list_One.add(x.next());
+            while(x.hasNext()){
+                list_One.add(x.next());
+            }
+
+            list_Two.add(y.next());
+            while(y.hasNext()){
+                list_Two.add(y.next());
+            }
+
+            //System.out.println("The size of List One is " + list_One.size());
+            //System.out.println("The size of List Two is " + list_Two.size());
+            
+            int size;
+
+            if(list_One.size() > list_Two.size()){
+                size = list_One.size();
+            }
+            else
+                size = list_Two.size();
+
+            int i = 0;
+            int j = 0;
+
+            //set initial markers
+            Marker_One = list_One.get(i);
+            //System.out.println("Marker_One is currently: "+ Marker_One);
+            Marker_Two = list_Two.get(j);
+            //System.out.println("Marker_Two is currently: "+Marker_Two);
+
+            while(i <= size || j <= size){
+                //System.out.println("At the beginning of the loop, Marker One is: " + Marker_One + " and Marker Two is: " + Marker_Two);
+                if(Marker_One.compareToIgnoreCase(Marker_Two) == 0){
+                    System.out.println(Marker_One);
+                    i++;
+                    j++;
+                    if(i == size || j == size)
+                        return;
+                    Marker_One = list_One.get(i);
+                    //System.out.println("There was just a match, Marker_One is currently: " + Marker_One +" and i is now " + i);
+                    Marker_Two = list_Two.get(j);
+                    //System.out.println("There was just a match and Marker_Two is currently: " + Marker_Two +" and j is now " + j);
+                }
+                else if(Marker_One.compareToIgnoreCase(Marker_Two) < 0){
+                    i++;
+                    if(i == size)
+                        return;
+                    Marker_One = list_One.get(i);
+                    //System.out.println("Marker_One came before Marker_Two and there was no match so Marker_One is now: "+ Marker_One +"and i is now " + i);
+                }
+                else{
+                    j++;
+                    if(j == size)
+                        return;
+                    Marker_Two = list_Two.get(j);
+                    //System.out.println("Marker_Two came before Marker_One and there was no match so Marker_Two is now: "+ Marker_Two +"and j is now " + j);
+                }
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+    }
 
     //main method
     public static void main(String[] args){
@@ -123,7 +212,7 @@ public class Lab01{
         findByBinarySearch(file, file_Two);
 
         //third search method call
-       // findByFinesse(file, file_Two);
+        findByFinesse(file, file_Two);
 
     }
     
