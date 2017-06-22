061e2d4e7042c0ba66e155baf866742b5a34969a
crosspatch <christine.d.lynch@gmail.com>
2017-06-22 12:53:51 -0400

    Still working on the HW

--- Hw01.java
+++ Hw01.java
@@ -1,6 +1,4 @@
 //CS2 HW 1
-//Christine Lynch
-//ch004919
 
 import java.io.*;
 import java.util.Scanner;
@@ -12,9 +10,17 @@ public class Hw01{
     public static void main(String[] args){
 
 
+        complexityIndicator();
+
         File file = null;
+        String filename;
+        int numOfOperations;
         if(0 < args.length){
-            file = new File(args[0]);
+            numOfOperations = Integer.parseInt(args[0]);
+            filename = args[1];
+            file = new File(filename);
+            System.out.println("The integer given is " +numOfOperations);
+            System.out.println("The name of the file given is " + filename);
         }
         else{
             System.out.println("Nothing there");
@@ -22,13 +28,15 @@ public class Hw01{
         }
 
         try{
-            
+            int counter = 0;
             Scanner input = new Scanner(file);
             while(input.hasNext()){
                 String letter = input.next();
                 String name = input.next();
-                System.out.printf("The current letter is %c and the current name is %s",letter,name);
+                System.out.printf("The current letter is %s and the current name is %s%n",letter,name);
+                counter++;
             }
+            System.out.println(counter);
         }
         catch(FileNotFoundException e){
             System.out.println("FIle not found");
@@ -39,29 +47,35 @@ public class Hw01{
 
 
     //methods needed
+    
+
+    public static void complexityIndicator(){
+
+        System.err.println("ch004919;5.0;20");
+    }
 
     //Method for insertion
-   /* public void hashInsert(){
+   /* public static void hashInsert(){
 
     }
 
     //Method for Deletion
-    public void hashDelete(){
+    public static void hashDelete(){
 
     }
     
     //Method for Searching through the Table
-    public void hashSearch(){
+    public static void hashSearch(){
 
     }
 
     //Method for Printing items
-    public void hashPrint(){
+    public static void hashPrint(){
 
     }
 
     //Method for quitting from current choice
-    public void hashQuit(){
+    public static void hashQuit(){
 
     }*/
 }
