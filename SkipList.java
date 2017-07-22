831d0ad28a5c73e2730267780cd38fe1537c373c
crosspatch <christine.d.lynch@gmail.com>
2017-07-22 19:30:14 -0400

    putting this with main file just in case I didn't copy everything right

--- SkipList.java
+++ SkipList.java
@@ -10,6 +10,61 @@ public class SkipList{
     //will house function calls and main the program based on user input
     public static void main(String[] args){
 
+        complexityIndicator();
+        //need to read from file now
+        File file = null;
+        String filename;
+        if(0 < args.length){
+            filename = args[0];
+            file = new File(filename);
+            System.out.println("For the input file named " + filename);
+        }
+        else{
+            System.out.println("Nothing there dude!");
+            System.exit(0);
+        }
+
+        try{
+            Scanner input = new Scanner(file);
+            //make new list
+            SkipList mySkip = new SkipList();
+            while(input.hasNextLine()){
+                String letter = input.next();
+                if(letter.equals("i") || letter.equals("d") || letter.equals("s")){
+
+                    int num = input.nextInt();
+                    switch(letter){
+                        boolean there = false;
+                        if(args[1] != null){
+                            there = true;
+                            System.out.println("With the RNG seeded,");
+                        }
+                        else
+                            System.out.println("With the RNG unseeded,");
+                        //options
+                        case "i": mySkip.insert(num,there);
+                                  break;
+                        case "d": mySkip.delete(num);
+                                  break;
+                        case "s": mySkip.search(num,'s',there);
+                                  break;
+                    }
+                }
+                else{
+                    System.out.println("the current Skip List is shown below:");
+                    switch(letter){
+                        case "p":mySkip.printAll();
+                                 break;
+                        default: System.out.println("Try again");
+                                 break;
+                    }
+                }
+
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
     }
 
 
@@ -230,6 +285,7 @@ public class SkipList{
     }
 
     public void printAll(){
+        
         //point to head...top of list
         SkipNodes currp = head;
         //pointing at -inf at the top
@@ -255,10 +311,14 @@ public class SkipList{
             }
             System.out.print("\n");
         }
+        System.out.println("---End of Skip List---");
 
 
 
     }
 
+    public void complexityIndicator(){
+        System.err.println("ch004919;5.0;20");
+    }
 }
 
