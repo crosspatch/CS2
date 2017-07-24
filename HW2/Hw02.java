030ddf9c56702ba941ac25dfe14aeff1bf378ba3
crosspatch <christine.d.lynch@gmail.com>
2017-07-23 22:40:03 -0400

    commented out a print line

--- HW2/Hw02.java
+++ HW2/Hw02.java
@@ -27,7 +27,7 @@ public class Hw02{
             //make new list
             SkipList mySkip = new SkipList();
             while(input.hasNextLine()){
-                String letter = input.next();
+                //String letter = input.next();
                 System.out.println(letter);
                 if(letter.equals("i") || letter.equals("d") || letter.equals("s")){
                     boolean there = false;
